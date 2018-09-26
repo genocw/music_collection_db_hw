badee5410c49a2d124f11fe84100678b7327c890
Genna Walsh <gennawalsh@gmail.com>
2018-09-26 15:58:19 +0100

    add album class

---
+++ models/album.rb
@@ -0,0 +1,12 @@
+class Album
+
+  attr_reader :id, :title, :genre, :artist_id
+
+  def initialize(options)
+    @id = options["id"].to_i
+    @title = options["title"]
+    @genre = options["genre"]
+    @artist_id = options["artist_id"].to_i
+  end
+
+end
